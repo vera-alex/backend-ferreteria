179d46231361213dec91495bcb5a249d15c62f4c
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-10 16:50:33 -0400

    creacion proyecto backend-ferreteria

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' })
+  app.enableCors();
+
+  await app.listen(process.env.PORT);
+  console.log(`App running on ${await app.getUrl()}`);
+}
+bootstrap();
