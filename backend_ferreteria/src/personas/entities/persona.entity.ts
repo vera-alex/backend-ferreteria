ce7bb1be1b97a5f810e1aea75f64a49f7fd5e212
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-16 17:33:01 -0400

    archivo personas.entity modificado

--- backend_ferreteria/src/personas/entities/persona.entity.ts
+++ backend_ferreteria/src/personas/entities/persona.entity.ts
@@ -1,1 +1,28 @@
-export class Persona {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('personas')
+export class Persona {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('varchar', { length: 50, nullable: false })
+  nombres: string;
+
+  @Column('varchar', { length: 20, nullable: true })
+  paterno: string;
+
+  @Column('varchar', { length: 20, nullable: true })
+  materno: string;
+
+  @Column('varchar', { length: 100, nullable: false })
+  direccion: string;
+
+  @Column('varchar', { length: 15, nullable: false })
+  telefono: string;
+
+  @CreateDateColumn({ name: 'fecha_creacion' })
+  fechaCreacion: Date;
+
+  @UpdateDateColumn({ name: 'fecha_modificacion' })
+  fechaModificacion: Date;
+}
