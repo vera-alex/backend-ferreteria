035b069acee7392905fb594e60615bdc5f992eeb
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-16 17:24:47 -0400

    actualizacion archivo README

--- backend_ferreteria/src/personas/personas.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PersonasService } from './personas.service';
-import { PersonasController } from './personas.controller';
-
-@Module({
-  controllers: [PersonasController],
-  providers: [PersonasService],
-})
-export class PersonasModule {}
