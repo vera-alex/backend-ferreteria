301e5f5aaa1a06ea8c2105e8ff8af809a564093c
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-17 15:22:01 -0400

    archivo personas.controller modificado

--- backend_ferreteria/src/personas/personas.controller.ts
+++ backend_ferreteria/src/personas/personas.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { PersonasService } from './personas.service';
 import { CreatePersonaDto } from './dto/create-persona.dto';
 import { UpdatePersonaDto } from './dto/update-persona.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('personas')
 @Controller('personas')
 export class PersonasController {
   constructor(private readonly personasService: PersonasService) {}
