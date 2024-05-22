bd90d7ed7f12b5e673e98b0f2c766a7a325bd899
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-22 00:23:24 -0400

    archivo empleados.service modificado

--- backend_ferreteria/src/empleados/empleados.service.ts
+++ backend_ferreteria/src/empleados/empleados.service.ts
@@ -1,26 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateEmpleadoDto } from './dto/create-empleado.dto';
 import { UpdateEmpleadoDto } from './dto/update-empleado.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Empleado } from './entities/empleado.entity';
+import { Repository } from 'typeorm';
 
+@ApiTags('empleados')
 @Injectable()
 export class EmpleadosService {
-  create(createEmpleadoDto: CreateEmpleadoDto) {
-    return 'This action adds a new empleado';
+  constructor(@InjectRepository(Empleado) private empleadosRepository: Repository<Empleado>) {}
+
+  async create(createEmpleadoDto: CreateEmpleadoDto): Promise<Empleado> {
+    const existe = await this.empleadosRepository.findOneBy({
+      persona: {
+        ci: createEmpleadoDto.idPersona,
+        nombres: createEmpleadoDto.idPersona,
+        paterno: createEmpleadoDto.idPersona,
+        materno: createEmpleadoDto.idPersona,
+      },
+    });
+
+    if (existe) {
+      throw new ConflictException('El empleado ya existe');
+    }
+
+    return this.empleadosRepository.save({
+      fechaContrato: createEmpleadoDto.fechaContrato,
+      persona: {
+        ci: createEmpleadoDto.idPersona,
+        nombres: createEmpleadoDto.idPersona,
+        paterno: createEmpleadoDto.idPersona,
+        materno: createEmpleadoDto.idPersona,
+        direccion: createEmpleadoDto.idPersona,
+        telefono: createEmpleadoDto.idPersona,
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all empleados`;
+  async findAll(): Promise<Empleado[]> {
+    return this.empleadosRepository.find({ relations: ['persona'] });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} empleado`;
+  async findOne(id: number): Promise<Empleado> {
+    const empleado = await this.empleadosRepository.findOneBy({ id });
+    if (!empleado) {
+      throw new NotFoundException(`El empleado ${id} no existe`);
+    }
+    return empleado;
   }
 
-  update(id: number, updateEmpleadoDto: UpdateEmpleadoDto) {
-    return `This action updates a #${id} empleado`;
+  async update(id: number, updateEmpleadoDto: UpdateEmpleadoDto): Promise<Empleado> {
+    const empleado = await this.findOne(id);
+    const empleadoUpdate = Object.assign(empleado, updateEmpleadoDto);
+    return this.empleadosRepository.save(empleadoUpdate);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} empleado`;
+  async remove(id: number) {
+    const empleado = await this.findOne(id);
+    return this.empleadosRepository.delete(empleado.id);
   }
 }
