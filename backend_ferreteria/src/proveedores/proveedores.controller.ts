914de6563628fad50faedae49893b1ab0fda513b
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-22 17:38:11 -0400

    archivo proveedores.controller.ts modificado

--- backend_ferreteria/src/proveedores/proveedores.controller.ts
+++ backend_ferreteria/src/proveedores/proveedores.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProveedoresService } from './proveedores.service';
 import { CreateProveedorDto } from './dto/create-proveedor.dto';
 import { UpdateProveedorDto } from './dto/update-proveedor.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('proveedores')
 @Controller('proveedores')
 export class ProveedoresController {
   constructor(private readonly proveedoresService: ProveedoresService) {}
