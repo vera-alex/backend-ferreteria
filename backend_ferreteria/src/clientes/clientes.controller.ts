4da20bbb11bca55c787495d028a25f08d341249f
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-22 13:11:53 -0400

    archivo clientes.controllers.ts modificado

--- backend_ferreteria/src/clientes/clientes.controller.ts
+++ backend_ferreteria/src/clientes/clientes.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ClientesService } from './clientes.service';
 import { CreateClienteDto } from './dto/create-cliente.dto';
 import { UpdateClienteDto } from './dto/update-cliente.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('clientes')
 @Controller('clientes')
 export class ClientesController {
   constructor(private readonly clientesService: ClientesService) {}
