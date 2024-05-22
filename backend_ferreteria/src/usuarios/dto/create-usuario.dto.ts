efbd0e5e2040a94167e190fb6ac3fde98c014d61
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-22 18:30:50 -0400

    archivo create-usuario.dto.ts modificado

--- backend_ferreteria/src/usuarios/dto/create-usuario.dto.ts
+++ backend_ferreteria/src/usuarios/dto/create-usuario.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUsuarioDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateUsuarioDto {
+  @ApiProperty()
+  @IsNotEmpty({ message: 'El campo usario no debe ser vacío' })
+  @IsString({ message: 'El campo usario debe ser de tipo cadena' })
+  @MaxLength(12, { message: 'El campo usario no debe ser mayor a 12 caracteres' })
+  @MinLength(4, { message: 'El campo usario no debe ser menor a 4 caracteres' })
+  readonly usuario: string;
+}
