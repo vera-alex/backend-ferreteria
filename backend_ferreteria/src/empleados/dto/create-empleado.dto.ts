4c044d67b8578470c6d1d7a57a17985eaed491ca
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-22 00:45:50 -0400

    se modifica el archivo empleado.dto

--- backend_ferreteria/src/empleados/dto/create-empleado.dto.ts
+++ backend_ferreteria/src/empleados/dto/create-empleado.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsDateString, IsNotEmpty } from "class-validator";
+import { IsDateString, IsDefined, IsNotEmpty, IsNumber } from "class-validator";
 
 export class CreateEmpleadoDto {
   @ApiProperty({ example: '2024-02-13' })
   @IsNotEmpty({ message: 'El campo fechaContrato no debe ser vacío' })
   @IsDateString({}, { message: 'El campo fechaContrato debe ser de tipo fecha' })
   readonly fechaContrato: Date;
+
+  @ApiProperty()
+  @IsDefined({ message: 'El campo idInterprete debe estar definido' })
+  @IsNumber({}, { message: 'El campo idInterprete debe ser de tipo numérico' })
+  readonly idPersona: number;
 }
