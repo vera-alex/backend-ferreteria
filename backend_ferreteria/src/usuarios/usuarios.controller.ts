8df083d88c11ab2253508bfa5b92cc48c956be4e
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-22 18:34:59 -0400

    archivo usuarios.controller.ts modificado

--- backend_ferreteria/src/usuarios/usuarios.controller.ts
+++ backend_ferreteria/src/usuarios/usuarios.controller.ts
@@ -1,8 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
+@ApiTags('usuarios')
 @Controller('usuarios')
 export class UsuariosController {
   constructor(private readonly usuariosService: UsuariosService) {}
