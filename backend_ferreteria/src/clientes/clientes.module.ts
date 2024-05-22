8301892c4ec360834bcb6c6990cb5b3a957d4e54
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-22 12:03:14 -0400

    archivo clientes.module.ts modificado

--- backend_ferreteria/src/clientes/clientes.module.ts
+++ backend_ferreteria/src/clientes/clientes.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ClientesService } from './clientes.service';
 import { ClientesController } from './clientes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cliente } from './entities/cliente.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Cliente])],
   controllers: [ClientesController],
   providers: [ClientesService],
 })
