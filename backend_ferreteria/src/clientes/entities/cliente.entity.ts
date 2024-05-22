f18f4913c9a749029bff079c0894f7153b93ea53
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-22 12:00:09 -0400

    archivo cliente.entity.ts modificado

--- backend_ferreteria/src/clientes/entities/cliente.entity.ts
+++ backend_ferreteria/src/clientes/entities/cliente.entity.ts
@@ -1,1 +1,18 @@
-export class Cliente {}
+import { Persona } from "src/personas/entities/persona.entity";
+import { CreateDateColumn, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('clientes')
+export class Cliente {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn({ name: 'fecha_creacion' })
+  fechaCreacion: Date;
+
+  @UpdateDateColumn({ name: 'fecha_modificacion' })
+  fechaModificacion: Date;
+
+  @OneToOne(() => Persona, persona => persona.clientes)
+  @JoinColumn({ name: 'id_persona', referencedColumnName: 'id' })
+  personas: Persona;
+}
