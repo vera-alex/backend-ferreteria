146310121bd272e01ceeabdfcf5e1a03bf202350
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-22 13:07:49 -0400

    archivo cliente.entity.ts modificado

--- backend_ferreteria/src/clientes/entities/cliente.entity.ts
+++ backend_ferreteria/src/clientes/entities/cliente.entity.ts
@@ -1,18 +1,31 @@
-import { Persona } from "src/personas/entities/persona.entity";
-import { CreateDateColumn, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity('clientes')
 export class Cliente {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column('varchar', { length: 15, nullable: false })
+  ci: string;
+
+  @Column('varchar', { length: 50, nullable: false })
+  nombres: string;
+
+  @Column('varchar', { length: 20, nullable: true })
+  paterno: string;
+
+  @Column('varchar', { length: 20, nullable: true })
+  materno: string;
+
+  @Column('varchar', { length: 50, nullable: true })
+  email: string;
+  
+  @Column('varchar', { length: 15, nullable: false })
+  celular: string;
+  
   @CreateDateColumn({ name: 'fecha_creacion' })
   fechaCreacion: Date;
 
   @UpdateDateColumn({ name: 'fecha_modificacion' })
   fechaModificacion: Date;
-
-  @OneToOne(() => Persona, persona => persona.clientes)
-  @JoinColumn({ name: 'id_persona', referencedColumnName: 'id' })
-  personas: Persona;
 }
