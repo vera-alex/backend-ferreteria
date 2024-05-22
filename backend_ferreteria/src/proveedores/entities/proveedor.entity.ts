1318d695f2c78b507241575f2e8d40899223d194
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-22 17:29:27 -0400

    archivo proveedor.entity.ts modificado

--- backend_ferreteria/src/proveedores/entities/proveedor.entity.ts
+++ backend_ferreteria/src/proveedores/entities/proveedor.entity.ts
@@ -1,4 +1,25 @@
-import { Entity } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity('proveedores')
-export class Proveedor {}
+export class Proveedor {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('varchar', { length: 50, nullable: false })
+  nombre: string;
+
+  @Column('varchar', { length: 70, nullable: false })
+  direccion: string;
+  
+  @Column('varchar', { length: 15, nullable: false })
+  telefono: string;
+
+  @Column('varchar', { length: 50, nullable: true })
+  email: string;
+
+  @CreateDateColumn({ name: 'fecha_creacion' })
+  fechaCreacion: Date;
+
+  @UpdateDateColumn({ name: 'fecha_modificacion' })
+  fechaModificacion: Date;
+}
