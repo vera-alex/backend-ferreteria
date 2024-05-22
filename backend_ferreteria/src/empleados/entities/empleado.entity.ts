ded79dcde179bff5bc336ab73cf32001fde4252d
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-21 23:55:03 -0400

    archivo empleado.entity modificado

--- backend_ferreteria/src/empleados/entities/empleado.entity.ts
+++ backend_ferreteria/src/empleados/entities/empleado.entity.ts
@@ -1,1 +1,16 @@
-export class Empleado {}
+import { CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('empleados')
+export class Empleado {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn({ name: 'fecha_contrato' })
+  fechaContrato: Date;
+
+  @CreateDateColumn({ name: 'fecha_creacion' })
+  fechaCreacion: Date;
+
+  @UpdateDateColumn({ name: 'fecha_modificacion' })
+  fechaModificacion: Date;
+}
