0e29c44a867019718cfc78b8721413730a7a1c12
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-22 18:39:38 -0400

    arreglo en el archivo usuarios.controller.ts

--- backend_ferreteria/src/usuarios/usuarios.controller.ts
+++ backend_ferreteria/src/usuarios/usuarios.controller.ts
@@ -3,6 +3,7 @@ import { UsuariosService } from './usuarios.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard)
