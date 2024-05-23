9b55d6617589de082d8d1be15288f1bc99bf40b4
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-22 20:54:05 -0400

    archivo categoria.entity.ts modificado

--- backend_ferreteria/src/categorias/entities/categoria.entity.ts
+++ backend_ferreteria/src/categorias/entities/categoria.entity.ts
@@ -1,1 +1,13 @@
-export class Categoria {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('categorias')
+export class Categoria {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('varchar', { length: 25, nullable: false })
+  nombre: string;
+
+  @Column('varchar', { length: 100, nullable: false })
+  descripcion: string;
+}
