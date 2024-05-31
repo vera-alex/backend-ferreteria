6a2d80eb9a3d138c15608d49e04b1218493393df
Brayan Serrudo <brayanserrudob@gmail.com>
2024-05-31 01:30:59 -0400

    modificacion en create-compras

--- backend_ferreteria/src/compras/dto/create-compra.dto.ts
+++ backend_ferreteria/src/compras/dto/create-compra.dto.ts
@@ -1,1 +1,14 @@
-export class CreateCompraDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsDefined, IsNumber } from "class-validator";
+
+export class CreateCompraDto {
+  @ApiProperty()
+  @IsDefined({ message: 'El campo idEmpleado debe estar definido' })
+  @IsNumber({}, { message: 'El campo idEmpleado debe ser de tipo numérico' })
+  readonly idUsuario: number;
+
+  @ApiProperty()
+  @IsDefined({ message: 'El campo idEmpleado debe estar definido' })
+  @IsNumber({}, { message: 'El campo idEmpleado debe ser de tipo numérico' })
+  readonly idProveedor: number;
+}
