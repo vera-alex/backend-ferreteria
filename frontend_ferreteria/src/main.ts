e4fb5dcad2c7b44f462205326fa0b4c3615aa497
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-06-12 10:34:31 -0400

    Se intala dependecias necesarias y modificación en el archivo main.ts

--- frontend_ferreteria/src/main.ts
+++ frontend_ferreteria/src/main.ts
@@ -1,7 +1,10 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faEdit, faPlus, faSave, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +13,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+library.add(faPlus, faEdit, faTrash, faSave)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
